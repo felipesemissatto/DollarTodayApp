598c2a5bf6a78d1d66fc5c80cdce87b9969bc4f5
Felipe Semissatto <felipesemi@gmail.com>
2021-02-07 14:13:14 -0300

    Initial project in XCode

---
+++ WhoPaidOneDollar/WhoPaidOneDollar/Util/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  WhoPaidOneDollar
+//
+//  Created by Felipe Semissatto on 07/02/21.
+//
+
+import Foundation
+
+let rootBackendURL = "https://gentle-hamlet-07840.herokuapp.com/api/v1/"
