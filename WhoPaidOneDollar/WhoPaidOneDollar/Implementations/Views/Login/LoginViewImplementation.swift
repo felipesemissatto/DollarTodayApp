d2aabea4f18068e8835e45fa92c3b96623288720
Felipe Semissatto <felipesemi@gmail.com>
2021-02-08 14:32:21 -0300

    setupTableView, referenceXib methods and tableview datasource and delega

--- WhoPaidOneDollar/WhoPaidOneDollar/Implementations/Views/Login/LoginViewImplementation.swift
+++ WhoPaidOneDollar/WhoPaidOneDollar/Implementations/Views/Login/LoginViewImplementation.swift
@@ -21,6 +21,7 @@ class LoginViewImplementation: UIView, LoginViewProtocol {
         self.viewController = viewController
         super.init(frame: CGRect.zero)
         initFromNib()
+        setupTableView()
     }
     
     required init?(coder: NSCoder) {
@@ -34,6 +35,32 @@ class LoginViewImplementation: UIView, LoginViewProtocol {
             nibView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
             addSubview(nibView)
         }
+    }
+    
+    // MARK: - Private methods
+    
+    /**
+     
+     Method that configs the table view.
+     
+     */
+    
+    private func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    /**
+     
+     Method that links the XIB with a cell by the identifier.
+     
+     - parameters nibName: the name of cell identifier.
+     
+     */
+    
+    private func referenceXib(nibName: String) {
+        let nib = UINib.init(nibName: nibName, bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: nibName)
     }
 }
 
@@ -49,7 +76,44 @@ extension LoginViewImplementation: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        var cellIdentifier = String()
+        var finalCell = UITableViewCell()
+        
+        switch indexPath.row {
+        case 0:
+            cellIdentifier = "PhotoTableViewCell"
+            referenceXib(nibName: cellIdentifier)
+            
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? PhotoTableViewCell  else {
+                fatalError("The dequeued cell is not an instance of PhotoTableViewCell.")
+            }
+            
+            finalCell = cell
+        case 1:
+            cellIdentifier = "TextFieldsTableViewCell"
+            referenceXib(nibName: cellIdentifier)
+            
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? TextFieldsTableViewCell  else {
+                fatalError("The dequeued cell is not an instance of TextFieldsTableViewCell.")
+            }
+            
+            finalCell = cell
+        case 2:
+            cellIdentifier = "ButtonSaveTableViewCell"
+            referenceXib(nibName: cellIdentifier)
+            
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? ButtonSaveTableViewCell  else {
+                fatalError("The dequeued cell is not an instance of ButtonSaveTableViewCell.")
+            }
+            
+            finalCell = cell
+            
+        default:
+            finalCell = UITableViewCell()
+            print("Unexpected: the cell is not a custom cell.")
+        }
+        
+        return finalCell
     }
 }
 
