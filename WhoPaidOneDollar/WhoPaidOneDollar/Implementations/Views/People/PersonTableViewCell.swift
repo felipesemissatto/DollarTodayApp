c5ca3cf7b2345fececd70ef6907cbc7ebd81caac
Felipe Semissatto <felipesemi@gmail.com>
2021-02-11 21:02:54 -0300

    persontableviewcell done

--- WhoPaidOneDollar/WhoPaidOneDollar/Implementations/Views/People/PersonTableViewCell.swift
+++ WhoPaidOneDollar/WhoPaidOneDollar/Implementations/Views/People/PersonTableViewCell.swift
@@ -9,11 +9,31 @@ import UIKit
 
 class PersonTableViewCell: UITableViewCell {
     // MARK: -IBOutlets
+    @IBOutlet weak var viewBackground: UIView!
+    @IBOutlet weak var imageViewProfilePic: UIImageView!
+    @IBOutlet weak var labelPersonName: UILabel!
     
+    @IBOutlet weak var viewTwitter: UIView!
+    @IBOutlet weak var labelTwitter: UILabel!
+    @IBOutlet weak var viewInstagram: UIView!
+    @IBOutlet weak var labelInstagram: UILabel!
+    
+    @IBOutlet weak var labelDate: UILabel!
     // MARK: -Lifecyle
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        // View background
+        viewBackground.layer.cornerRadius = 6
+        viewBackground.layer.shadowColor = UIColor.black.cgColor
+        viewBackground.layer.shadowOpacity = 0.25
+        viewBackground.layer.shadowOffset = CGSize(width: 0, height: 4)
+        viewBackground.layer.shadowRadius = 1
+        viewBackground.layer.masksToBounds = false
+        
+        // Image profile pic
+        imageViewProfilePic.clipsToBounds = true
+        imageViewProfilePic.layer.cornerRadius = 0.5 * imageViewProfilePic.bounds.size.width
     }
 }
 
